0d3d4a401ab16f67d0e0252e3520aad3125376b5
Rui <lirui.jobs@gmail.com>
2020-05-28 17:55:43 +1000

    set numbers in the grids sucessfully

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-// put random number into each grid-item
+// 2 * 2 grid
 const numArr = [1, 2, 3, 4];
-const indexOfNumArr = Math.floor(Math.random() * 4);
-const randomNum = numArr[indexOfNumArr];
-document.getElementById("grid-item1").innerHTML = randomNum;
-const setRandomNum = () => {};
+
+// get a random number
+const getRandomNum = () => {
+  const indexOfNumArr = Math.floor(Math.random() * 4);
+  return numArr[indexOfNumArr];
+};
+
+// put the different random numbers into each grid
+const setDiffRandomNumToEachGrid = () => {
+  let elements = document.getElementsByClassName("grid-item");
+  let randomNumArr = [];
+  for (let i = 0; i < 4; i++) {
+    let randomNum = getRandomNum();
+
+    while (randomNumArr.includes(randomNum)) {
+      randomNum = getRandomNum();
+    }
+    randomNumArr.push(randomNum);
+
+    elements[i].innerHTML = randomNumArr[i];
+  }
+};
+setDiffRandomNumToEachGrid();
+
+//
